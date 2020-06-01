648e7a66d11e1e91a222cc85e3037399b8332153
sonota88 <yosiot8753@gmail.com>
2020-06-02 08:52:19 +0900

    __wip 2020-06-02 08:52:19 command client retry

--- lib/digdag_utils/client/command_client.rb
+++ lib/digdag_utils/client/command_client.rb
@@ -21,6 +21,37 @@ module DigdagUtils
           DigdagUtils::Session.from_command_output(block)
         }
       end
+
+      # revision: :latest | :keep
+      # resume: :resume | :all
+      def retry(
+        attempt_id,
+        revision: :keep,
+        resume: :resume
+      )
+        args = ["digdag", "retry", attempt_id]
+
+        args <<
+          case revision
+          when :keep then "--keep-revision"
+          when :latest then "--latest-revision"
+          else
+            raise "invalid"
+          end
+
+        args <<
+          case resume
+          when :resume then "--resume"
+          when :all then "--all"
+          else
+            raise "invalid"
+          end
+
+        args += ["-e", @endpoint]
+        pp args
+
+        # DigdagUtils.system(*args)
+      end
     end
   end
 end
--- sample.rb
+++ sample.rb
@@ -2,7 +2,13 @@ require "digdag_utils/client/command_client"
 
 client = DigdagUtils::Client::CommandClient.new(endpoint: "localhost:6543")
 
-ss = client.get_sessions(
-  "proj",
-  "wf"
+# ss = client.get_sessions(
+#   "proj",
+#   "wf"
+# )
+
+client.retry(
+  123,
+  revision: :latest,
+  resume: :all
 )
