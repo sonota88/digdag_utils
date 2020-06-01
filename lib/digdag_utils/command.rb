6678bc158c661595c71820a82e3e7a4cbf891c24
sonota88 <yosiot8753@gmail.com>
2020-06-02 08:14:40 +0900

    __wip 2020-06-02 08:14:40 command get_session

--- lib/digdag_utils/command.rb
+++ lib/digdag_utils/command.rb
@@ -1,4 +1,22 @@
 module DigdagUtils
   class Command
+    def initialize(endpoint: nil)
+      @endpoint = endpoint
+    end
+
+    def get_sessions(pj, wf)
+      out =
+        DigdagUtils.system(
+          "digdag",
+          "sessions",
+          pj, wf,
+          "-e", @endpoint
+        )
+
+      blocks = out.split("\n\n")
+      blocks.map { |block|
+        DigdagUtils::Session.from_command_output(block)
+      }
+    end
   end
 end
