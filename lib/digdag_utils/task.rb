48a7e49c0e7542692d9cdaeda621a9a143957167
sonota88 <yosiot8753@gmail.com>
2020-06-03 07:48:11 +0900

    Add Task#state

--- lib/digdag_utils/task.rb
+++ lib/digdag_utils/task.rb
@@ -1,18 +1,21 @@
 module DigdagUtils
   class Task
-    attr_reader :id, :name
+    attr_reader :id, :name, :state
 
     def initialize(
       id: nil,
-      name: nil
+      name: nil,
+      state: nil
     )
       @id = id
       @name = name
+      @state = state
     end
 
     def self.from_command_output(block)
       id = nil
       name = nil
+      state = nil
 
       if m = block.match(/  id: (\d+)/)
         id = m[1].to_i
@@ -20,10 +23,14 @@ module DigdagUtils
       if m = block.match(/  name: (.+)/)
         name = m[1]
       end
+      if m = block.match(/  state: (.+)/)
+        state = m[1]
+      end
 
       Task.new(
         id: id,
         name: name,
+        state: state
       )
     end
   end
