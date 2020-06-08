5306f36702676cc9b7503a408e15093517c9a716
sonota88 <yosiot8753@gmail.com>
2020-06-09 07:07:31 +0900

    Add properties (task)

--- lib/digdag_utils/task.rb
+++ lib/digdag_utils/task.rb
@@ -1,15 +1,28 @@
 module DigdagUtils
   class Task
     attr_reader :id, :name, :state
+    attr_reader :full_name
+    attr_reader :parent_id, :upstreams
+    attr_reader :is_group
 
     def initialize(
       id: nil,
       name: nil,
-      state: nil
+      state: nil,
+      full_name:        nil,
+      cancel_requested: nil,
+      parent_id:        nil,
+      upstreams:        nil,
+      is_group:         nil
     )
       @id = id
       @name = name
       @state = state
+      @full_name        = full_name
+      @cancel_requested = cancel_requested
+      @parent_id        = parent_id
+      @upstreams        = upstreams
+      @is_group         = is_group
     end
 
     def self.from_command_output(block)
