4c94a261e750c520347acd19a026df0684b885b2
sonota88 <yosiot8753@gmail.com>
2020-06-09 08:09:44 +0900

    Add Attempt#session

--- lib/digdag_utils/attempt.rb
+++ lib/digdag_utils/attempt.rb
@@ -1,25 +1,39 @@
 module DigdagUtils
   class Attempt
     attr_reader :id
+    attr_reader :session
 
     def initialize(
       id: nil,
       done:             nil,
       success:          nil,
-      cancel_requested: nil
+      cancel_requested: nil,
+      session:          nil
     )
       @id = id
       @done             = done
       @success          = success
       @cancel_requested = cancel_requested
+      @session          = session
     end
 
     def self.from_api_response(data)
+      wf = Workflow.new(
+        id:   data["workflow"]["id"],
+        name: data["workflow"]["name"]
+      )
+
+      sess = Session.new(
+        id: data["sessionId"],
+        workflow: wf
+      )
+
       new(
         id:               data["id"],
         done:             data["done"],
         success:          data["success"],
-        cancel_requested: data["cancelRequested"]
+        cancel_requested: data["cancelRequested"],
+        session:          sess
       )
     end
 
@@ -29,6 +43,7 @@ module DigdagUtils
         done:             @done,
         success:          @success,
         cancel_requested: @cancel_requested,
+        session:          @session.to_plain,
       }
     end
   end
